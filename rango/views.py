e048bdcbe41032b398be5a822111cf1208c47ab7
Martin Suchan <2572779s@student.gla.ac.uk>
2022-01-22 18:03:43 +0000

    Minor url/hyperlink fixes to pass the chap3 autotest

--- rango/views.py
+++ rango/views.py
@@ -5,6 +5,6 @@ from django.shortcuts import redirect
 def redir(request):
     return redirect('/rango')
 def index(request):
-    return HttpResponse("""Rango says hey there partner! <a href="about">About.</a>""")
+    return HttpResponse("""Rango says hey there partner! <a href="/rango/about">About</a>""")
 def about(request):
-    return HttpResponse("""Rango says here is the about page. <a href="/rango">Index.</a>""")
+    return HttpResponse("""Rango says here is the about page. <a href="/rango/">Index</a>""")
