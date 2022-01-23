e8c724fa4069b190002930aab0af7a494ab55beb
Martin Suchan <2572779s@student.gla.ac.uk>
2022-01-23 17:01:35 +0000

    CHAP 4 DEFINITIVE | Added about template, amended view, added media cat.jpg and added it to about template, tweaks to pass CHAP4 tests

--- rango/views.py
+++ rango/views.py
@@ -15,4 +15,5 @@ def index(request):
 
 
 def about(request):
-    return HttpResponse("""Rango says here is the about page. <a href="/rango/">Index</a>""")
+    context_dict = {'boldmessage' : 'This tutorial has been put together by Martin.'}
+    return render(request, 'rango/about.html', context=context_dict)
