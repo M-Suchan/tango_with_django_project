742c4b06e54f4b7e10ae5fee4c3c1bac84d11d44
Martin Suchan <2572779s@student.gla.ac.uk>
2022-01-23 16:10:49 +0000

    CHAP 4 | Created Dynamic path to templates, create a template for index called index.html and amended views.py to use template, added /static/ and /static/images to house our image files, added rango.jpg and amended index.html template, added support for media into settings.py and added a /media/ folder, amended settings.py with path variables to reflect new files.

--- rango/views.py
+++ rango/views.py
@@ -4,7 +4,15 @@ from django.shortcuts import redirect
 
 def redir(request):
     return redirect('/rango')
+
+
 def index(request):
-    return HttpResponse("""Rango says hey there partner! <a href="/rango/about">About</a>""")
+    # Create a context dictionairy to give context to {{ msg }} template variables in the html file
+    # Usage : for {{msgname}} in html >>> 'msgname' : 'msgcontent' for each key : value pair
+    context_dict = {'boldmessage' : 'Crunchy, creamy, cookie, candy, cupcake!'}
+    # Return our rendered response
+    return render(request, 'rango/index.html', context=context_dict)
+
+
 def about(request):
     return HttpResponse("""Rango says here is the about page. <a href="/rango/">Index</a>""")
