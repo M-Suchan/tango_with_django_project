4f07cd7a4068f46a7113bfe535ec89cc73224985
Martin Suchan <2572779s@student.gla.ac.uk>
2022-02-07 16:08:59 +0000

    CHAP 5| Added views,likes fields to Cats, changed populate.py accordingly, with dummy like and view values as per Exec instructions

--- rango/models.py
+++ rango/models.py
@@ -1,9 +1,11 @@
+from email.policy import default
 from django.db import models
 
 # Category model
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
-
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
     class Meta:
         verbose_name_plural = 'Categories'
 
