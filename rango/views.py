4b180ab7b05d9a51d0bfa404899f2e8bfbc0611e
Martin Suchan <2572779s@student.gla.ac.uk>
2022-02-07 19:43:06 +0000

    CHAP 6| modified Views.py to show categories on index page, modified index template to use list of categories and show top5, added a slug field and use of slugify to model of Category

--- rango/views.py
+++ rango/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import redirect
+from rango.models import Category
 
 def redir(request):
     return redirect('/rango')
 
 
 def index(request):
+    category_list = Category.objects.order_by('-likes')[:5]
     # Create a context dictionairy to give context to {{ msg }} template variables in the html file
     # Usage : for {{msgname}} in html >>> 'msgname' : 'msgcontent' for each key : value pair
-    context_dict = {'boldmessage' : 'Crunchy, creamy, cookie, candy, cupcake!'}
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
     # Return our rendered response
     return render(request, 'rango/index.html', context=context_dict)
 
