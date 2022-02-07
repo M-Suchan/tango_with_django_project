3a8020103428ec5957f9ff6083b28a8af0fa7bc2
Martin Suchan <2572779s@student.gla.ac.uk>
2022-02-07 15:23:09 +0000

    CHAP 5 | Commiting changes to models.py, admin.py as well as adding a populate_rango.py script, finally, attempting to fix a merge/branch issue that I caused by typing git init into an already cloned repo

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+# Category model
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+# Page model
+class Page(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
