3647ee92e6c6a5fa97301257018454663b1b7df8
Martin Suchan <2572779s@student.gla.ac.uk>
2022-02-08 15:03:33 +0000

    CHAP 8 DEFINITIVE| Completed all of Chap8, all exercises OK'd

--- rango/views.py
+++ rango/views.py
@@ -6,7 +6,7 @@ from rango.forms import CategoryForm, PageForm
 from django.urls import reverse
 
 def redir(request):
-    return redirect('/rango')
+    return redirect(reverse('rango:index'))
 
 
 def index(request):
@@ -50,7 +50,7 @@ def add_category(request):
 
         if form.is_valid():
             form.save(commit=True)
-            return redirect('/rango/')
+            return redirect(reverse('rango:index'))
         else:
             print(form.errors)
     return render(request, 'rango/add_category.html', {'form': form})
@@ -61,7 +61,7 @@ def add_page(request, category_name_slug):
     except Category.DoesNotExist:
         category = None
     if category is None:
-        return redirect('/rango/')
+        return redirect(reverse('rango:index'))
     form = PageForm()
 
     if request.method == 'POST':
