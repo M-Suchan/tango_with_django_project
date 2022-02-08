0ef58b09593ebbfdc0a35dcda14ef194aba8b324
Martin Suchan <2572779s@student.gla.ac.uk>
2022-02-08 00:50:10 +0000

    CHAP 7 DEFINITIVE| Sole chapt. 7 commit, all tests OK'd

--- rango/views.py
+++ rango/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import redirect
 from rango.models import Category, Page
+from rango.forms import CategoryForm, PageForm
+from django.urls import reverse
 
 def redir(request):
     return redirect('/rango')
@@ -39,3 +41,43 @@ def show_category(request, category_name_slug):
 def about(request):
     context_dict = {'boldmessage' : 'This tutorial has been put together by Martin.'}
     return render(request, 'rango/about.html', context=context_dict)
+
+def add_category(request):
+    form = CategoryForm()
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return redirect('/rango/')
+        else:
+            print(form.errors)
+    return render(request, 'rango/add_category.html', {'form': form})
+
+def add_page(request, category_name_slug):
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+        category = None
+    if category is None:
+        return redirect('/rango/')
+    form = PageForm()
+
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+
+        if form.is_valid():
+            if category:
+                page = form.save(commit=False)
+                page.category = category
+                page.views = 0
+                page.save()
+
+                return redirect(reverse('rango:show_category',
+                                        kwargs={'category_name_slug':
+                                        category_name_slug}))
+        else:
+            print(form.errors)
+    context_dict = {'form':form, 'category':category}
+    return render(request, 'rango/add_page.html', context=context_dict)
