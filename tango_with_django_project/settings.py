a6e170a79f964b161388bdca8c2dd4419fa80c84
Martin Suchan <2572779s@student.gla.ac.uk>
2022-02-09 14:16:35 +0000

    CHAP 10 DEFINITIVE| Cookies are now server side, visits counter at the about page, all execs autotests OK'd

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -54,6 +54,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+MIDDLEWARE_CLASSES = [
+    'django.contrib.sessions.middleware.SessionMiddleware',
+]
+
 ROOT_URLCONF = 'tango_with_django_project.urls'
 
 TEMPLATES = [
